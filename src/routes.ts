dfb69617afeca319656d5445c0e61b0aebc3bc4c
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2025-04-18 10:22:17 -0300

    Atualizar o routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -9,14 +9,18 @@ const routes = express.Router();
 const roomsController = new RoomsController();
 const eventsController = new EventsController();
 
+//endpoint de get 
 routes.get('/rooms', roomsController.index);
+//endpoint post 
 routes.post('/rooms', celebrate({
     body: Joi.object().keys({
     name: Joi.string().required().max(50),
     building: Joi.string().required().max(50)
 })}, { abortEarly: false }),
 roomsController.create);
+//endpoint put
 routes.put('/rooms/:id_room', roomsController.update);
+//endpoint get para fazer update na data 
 routes.get('/rooms/:id_room', roomsController.dataUpdate);
 
 routes.get('/events', eventsController.index);
@@ -29,7 +33,9 @@ routes.post('/events', celebrate({
     date_time: Joi.string().required(),
     rooms: Joi.array().required()
 })}, { abortEarly: false }), eventsController.create);
+//end point deletar eventos da salab
 routes.delete('/events/:id_event', eventsController.remove);
+//end point deletar data por id 
 routes.get('/events/:id_event', eventsController.dataRemove);
 
 export default routes;
