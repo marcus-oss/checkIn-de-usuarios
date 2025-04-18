48ac6bd82bad5f6ab0e3d5d27f2ecc555d5fab72
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2025-04-18 10:23:17 -0300

    Atualizar o create_room.ts

--- src/Seed/create_room.ts
+++ src/Seed/create_room.ts
@@ -1,5 +1,6 @@
 import Knex from 'knex';
 
+//criando uma sala 
 export async function seed (knex: Knex){
     await knex('rooms').insert([
         { name: 'Teste a sala', building: 'Teste criado!!'}
